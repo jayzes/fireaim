c0f3a2d9a1aee0b0a7689246d82d4c587cdb40d7
Jay Zeschin <jay@zeschin.org>
2011-09-19 23:51:18 -0600

    Add key support

--- stream.rb
+++ stream.rb
@@ -1,6 +1,7 @@
 require 'goliath'
 require 'tempfile'
 require 'yaml'
+require 'cgi'
 
 class Stream < Goliath::API
   
@@ -11,15 +12,19 @@ class Stream < Goliath::API
     revision    = params[:revision] || 'master'
     environment = params[:environment]
     app         = params[:app]
+    key         = params[:key]
+  
+    return [ 422, {}, [ 'api_token, app, key, and environment parameters are required' ] ] unless api_token && environment && app && key
     
-    return [ 422, {}, [ 'api_token, app, and environment parameters are required' ] ] unless api_token && environment && app
+    creds_tempfile = Tempfile.new('creds')
+    creds_tempfile.write({'api_token' => api_token}.to_yaml)
+    creds_tempfile.close
     
-    tempfile = Tempfile.new('creds')
-    tempfile.write({'api_token' => api_token}.to_yaml)
-    tempfile.close
+    key_tempfile = Tempfile.new('key')
+    key_tempfile.write(CGI.unescape(key))
+    key_tempfile.close
     
-    ENV['EYRC'] = tempfile.path
-    cmd = "ey deploy -a #{app} -e #{environment} -r #{revision}"
+    cmd = "ssh-agent sh -c 'ssh-add #{key_tempfile.path} < /dev/null && EYRC=#{creds_tempfile.path} ey deploy -a #{app} -e #{environment} -r #{revision}'"
     
     # Set a timeout
     EM.add_timer(90) do
@@ -31,7 +36,8 @@ class Stream < Goliath::API
     EM.system(cmd) do |output,status|
       env.stream_send("#{output}\n")
       if status.exited?
-        tempfile.unlink
+        creds_tempfile.unlink
+        key_tempfile.unlink
         env.stream_close
       end
     end
